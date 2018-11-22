297c14b87df8df8e923c021b3143f9ae62f9b2f0
dingbenfa <1406731195@qq.com>
2018-11-23 00:52:48 +0800

    商品详情页面功能开发

--- pages/goods/goodsDetails/goodsDetails.js
+++ pages/goods/goodsDetails/goodsDetails.js
@@ -10,10 +10,40 @@ Page({
     circular: false,
     atLeastReducePrice: 0,
     goodsEvaluation: goodsDetailInfo.goodsEvaluation,
-    goodsEvaluationLimit: 2
+    goodsEvaluationLimit: 2,
+    isActive: 1, //默认选择商品
+    scrollCls: {
+      evaluation: 0,
+      detail: 0
+    },
+    toView: "swiperSection",
+    animationData: {},
+    showModalStatus: true
   },
   onLoad: function(options) {
     this.handleAtLeastReduce();
+  },
+  onReady: function() {
+    this.queryThePointNodes();
+  },
+  scroll: function(e) {
+    // console.log(e.detail.scrollTop);
+    let scrollTopVal = e.detail.scrollTop;
+    let evaluationVal = this.data.scrollCls.evaluation;
+    let detailVal = this.data.scrollCls.detail;
+    if (scrollTopVal > evaluationVal && scrollTopVal < detailVal) {
+      this.setData({
+        isActive: 2
+      });
+    } else if (scrollTopVal > detailVal) {
+      this.setData({
+        isActive: 3
+      });
+    } else {
+      this.setData({
+        isActive: 1
+      });
+    }
   },
   //领券至少可减
   handleAtLeastReduce() {
@@ -32,7 +62,7 @@ Page({
     });
   },
   //查看全部评论
-  handleLookUpAll(){
+  handleLookUpAll() {
     console.log("查看全部评论");
   },
   //查看更多评论
@@ -49,5 +79,96 @@ Page({
         goodsEvaluationLimit: newLimit
       });
     }
+  },
+  //商品详情锚点
+  goToPoint(ev) {
+    let pType = Number(ev.currentTarget.dataset.pointtype);
+    this.setData({
+      isActive: pType
+    });
+
+    if (pType === 2) {
+      this.setData({
+        toView: "goodsEvaluation"
+      })
+    } else if (pType === 3) {
+      this.setData({
+        toView: "goodsDetail"
+      })
+    } else {
+      this.setData({
+        toView: "swiperSection"
+      })
+    }
+  },
+  //获取锚点距离顶部高度
+  queryThePointNodes() {
+    const self = this;
+
+    const query = wx.createSelectorQuery()
+    query.select('.m-goods-evaluation').boundingClientRect()
+    query.selectViewport().scrollOffset()
+    query.exec(function(res) {
+      let str = "scrollCls.evaluation";
+      self.setData({
+        [str]: res[0].top - 50
+      });
+    })
+
+    const queryOther = wx.createSelectorQuery()
+    queryOther.select('.m-goods-detail').boundingClientRect()
+    queryOther.selectViewport().scrollOffset()
+    queryOther.exec(function(res) {
+      let str = "scrollCls.detail";
+      self.setData({
+        [str]: res[0].top - 50
+      });
+    })
+  },
+  goToCart(ev) {
+    wx.switchTab({
+      url: '../../cart/index'
+    })
+  },
+  //领券
+  openCouponsModal: function() {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  closeCouponsModal: function() {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function() {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
   }
 })
